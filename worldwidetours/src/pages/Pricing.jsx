ebf430e631342d47a4f63f23e56213787ae798ae
Sean Pivin <seanpivin@gmail.com>
2023-08-03 11:23:13 +0300

    finish the pricing, product and about comp and pages

--- worldwidetours/src/pages/Pricing.jsx
+++ worldwidetours/src/pages/Pricing.jsx
@@ -12,15 +12,59 @@ export default function Product() {
           <h2>
             Simple pricing.
             <br />
-            Just $9/month.
+            Just $29.99/month. Cancel anytime.
           </h2>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
-            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
-            voluptatem iste.
+            At our travel website, we offer an exciting and convenient way for
+            travelers to explore their dream destinations. Our pricing model is
+            designed to provide you with the best value for your money while
+            ensuring a seamless and enjoyable experience throughout your
+            journey. For only <strong>$29.99 a month</strong> , you gain access
+            to a comprehensive range of features and benefits:
+            <br />
+            <p>
+              1) Interactive Map Selection: Choose your travel destination with
+              ease by simply clicking on the interactive map. Our platform makes
+              it simple to explore various locations worldwide, giving you the
+              freedom to plan your adventures effortlessly.
+            </p>
+            <p>
+              2) Personalized Itineraries: Tailor-made travel itineraries are at
+              your fingertips. We curate unique experiences based on your
+              preferences, interests, and budget, ensuring that each trip is a
+              memorable one.
+            </p>
+            <p>
+              3) Real-time Travel Updates: Stay informed with real-time updates
+              on travel advisories, weather conditions, and any other critical
+              information that may impact your trip. Our commitment to safety
+              and convenience means you can make well-informed decisions at all
+              times. Community and Reviews: Engage with fellow travelers, share
+              your experiences, and read honest reviews of destinations and
+              accommodations from our vibrant community. Get insider tips and
+              recommendations to make the most of your travels.
+            </p>
+            <p>
+              4)24/7 Customer Support: Our dedicated customer support team is
+              available round-the-clock to assist you with any queries or
+              concerns you may have during your travels. Rest assured, we've got
+              your back wherever you go. Join us on this adventure of a lifetime
+              for just $29.99 a month. Embark on your dream getaways and let us
+              be your travel companion every step of the way.
+            </p>
+            4)24/7 Customer Support: Our dedicated customer support team is
+            available round-the-clock to assist you with any queries or concerns
+            you may have during your travels.
+            <p>
+              <br />
+              Rest assured, we've got your back wherever you go. Join us on this
+              adventure of a lifetime for just $29.99 a month. Embark on your
+              dream getaways and let us be your travel companion every step of
+              the way.
+            </p>
           </p>
         </div>
-        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+        <img src="seanPivin.jpeg" alt="This is me in Hong-Kong" />
       </section>
     </main>
   );
--- worldwidetours/src/pages/Product.jsx
+++ worldwidetours/src/pages/Product.jsx
@@ -7,22 +7,46 @@ export default function Product() {
       <PageNav />
 
       <section>
-        <img
-          src="img-1.jpg"
-          alt="person with dog overlooking mountain with sunset"
-        />
+        <img src="Rotem&the boys.jpg" alt="My Life :)" />
         <div>
           <h2>About WorldWide.</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est
-            dicta illum vero culpa cum quaerat architecto sapiente eius non
-            soluta, molestiae nihil laborum, placeat debitis, laboriosam at fuga
-            perspiciatis?
+            Welcome to World Wide! We are passionate travel enthusiasts
+            committed to transforming the way you explore the world. Our
+            platform was born out of a shared love for wanderlust and the desire
+            to make travel planning a delightful experience for everyone.
           </p>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
-            doloribus libero sunt expedita ratione iusto, magni, id sapiente
-            sequi officiis et.
+            At World Wide, we believe that travel is more than just visiting new
+            places. It's about immersing yourself in diverse cultures, creating
+            unforgettable memories, and discovering the hidden gems that make
+            every journey unique.
+          </p>
+          <p>
+            Our team of experienced full-stack developers, specializing in the
+            MERN stack, has meticulously crafted this platform to be your
+            ultimate travel companion. With a user-friendly interface and
+            cutting-edge technology, we aim to simplify the way you plan, book,
+            and experience your vacations.
+          </p>
+          <p>
+            Whether you're an intrepid adventurer seeking thrilling escapades or
+            a laid-back explorer looking for tranquil retreats, we have
+            something special in store for you. Discover new destinations on our
+            interactive map, personalize your travel itineraries, and connect
+            with like-minded travelers who share your passion for discovery.
+          </p>
+          <p>
+            With safety, convenience, and exceptional customer service at the
+            heart of our mission, we strive to be your trusted partner in every
+            travel endeavor. Our affordable pricing model ensures that embarking
+            on your dream vacations is no longer a distant dream.
+          </p>
+          <p>
+            <strong>
+              Thank you for joining us on this exciting journey. Let's explore
+              the world together, one destination at a time!
+            </strong>
           </p>
         </div>
       </section>
